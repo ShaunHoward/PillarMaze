742569047ae81f0f2607d48b3b14bdc4516e4a4d
ShaunHoward <smh150@case.edu>
2014-10-12 23:20:27 -0400

    Halfway through P*.

--- src/maze/MazeSolver.java
+++ src/maze/MazeSolver.java
@@ -1,60 +1,113 @@
 package maze;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by shaun on 10/12/14.
  */
 public class MazeSolver {
+
+    /**
+     * P* algorithm which finds the most optimal path to from
+     * the beginning pillar to the end pillar of the given maze.
+     * Unlike A*, P* considers all pillars with extra planks between
+     * neighbors, given that the number of planks at the current step
+     * are greater than 0.
+     *
+     * Once P* begins to follow a shortest path, it takes that path
+     * to the end of the maze.
+     *
+     * @param maze - the maze to find the shortest path through
+     * @param size - the expected size of the maze
+     */
+    static List<Pillar> pStar(Maze maze, int size) throws Exception {
+        /* if the size of maze is not equal to size */
+        if (maze.size != size) {
+            /* throw an invalid size exception */
+            throw new Exception("Maze size was unequal to expected size.");
+        }
+
+        /* initialize the set of expanded nodes 'E' */
+        Set<Pillar> E = new HashSet<>();
+
+        /*
+         * initialize a priority queue of nodes 'N' sorted in non-decreasing order by cost, c,
+         * the sum of heuristic and number of times moved
+        */
+        PriorityQueue<Pillar> N = new PriorityQueue<>();
+
+        /* if maze size is greater than 0 */
+        if (size > 0) {
+
+            /* The beginning pillar of the maze. */
+            Pillar pillar = maze.getBegin();
+
+           /* add the beginning node of maze to E */
+            E.add(pillar);
+
+           /* add the beginning node to N with c=0, n=1, e=distance to end from beginning */
+            pillar.setCost(0);
+            pillar.setPlanksLeft(1);
+            pillar.setDistanceFromBegin(distanceToEnd(pillar));
+            N.add(pillar);
+        }
+
+        /* while N is not empty */
+        while (!N.isEmpty()) {
+
+            /* extract lowest cost node 'v' from N */
+            Pillar v = N.poll();
+
+            /* if v is the end node of maze */
+            if (v.equals(maze.getEnd())) {
+                /* return shortest path to v */
+                return shortestPath(v);
+            }
+
+            /* find the connected neighbor nodes of v */
+            List<Pillar> connectedNeighbors = v.getNeighborList(true);
 /*
-    //P* algorithm
-    call input graph 'maze'
-    call input length 'size'
-            if the size of maze is not equal to size
-    throw an invalid size exception
-    end if
-    initialize the set of expanded nodes 'E'
-    initialize a priority queue of nodes 'N' sorted in non-decreasing order by cost, c,
-    the sum of heuristic and number of times moved
-    if maze size is greater than 0
-    add the beginning node of maze to E
-    add the beginning node to N with c=0, n=1, e=distance to end from beginning
-    end if
-            while N is not empty
-    extract lowest cost node 'v' from N
-    if v is the end node of maze
-    return shortest path to v
-    end if
-    find the connected neighbor nodes of v
-    for each connected node 'c'
-            if c does not exist in E
-    c.p . v
-    c.b . v.b + 1
-    c.e . distance to end from c
-    c.n . v.n
-    c.c . c.b + c.e
-    add c to E
-    add c to N
-    end if
-    end
+        for each connected node 'c'
+                if c does not exist in E
+        c.p . v
+        c.b . v.b + 1
+        c.e . distance to end from c
+        c.n . v.n
+        c.c . c.b + c.e
+        add c to E
+        add c to N
+        end if */
+        }
+/*
+        end
 
-    if v.n > 0
-    find the unconnected neighbor nodes of v
-    for each unconnected node u
-    if u does not exist in E
-    u.p . v
-    u.b . v.b + 1
-    u.e . distance to end from u
-    u.n . v.n - 1
-    u.c . u.b + u.e
-    add u to E
-    add u to N
-    end if
-    end
-    end if
-    end
-    return nil
+        if v.n > 0
+        find the unconnected neighbor nodes of v
+        for each unconnected node u
+        if u does not exist in E
+        u.p . v
+        u.b . v.b + 1
+        u.e . distance to end from u
+        u.n . v.n - 1
+        u.c . u.b + u.e
+        add u to E
+        add u to N
+        end if
+        end
+        end if
+        end
+        return nil */
+    }
 
+    private static List<Pillar> shortestPath(Pillar v) {
+        return null;
+    }
+
+    private static float distanceToEnd(Pillar pillar) {
+        return 0;
+    }
+
+    /*
     // shortest path to end
     initialize the path list
     initialize a node called 'curr' with end
